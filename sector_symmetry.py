73149aa73558e6fdc064730043fe7f70fb61367c
yasuomaidana <yasuomaidana@gmail.com>
2023-04-14 22:50:48 +0900

    COUR-982: Showing symmetry

--- sector_symmetry.py
+++ sector_symmetry.py
@@ -2,6 +2,7 @@ from fractions import Fraction
 from typing import List, Tuple
 from transformations.Clarke import clark
 import numpy as np
+import matplotlib.pyplot as plt
 
 delta_u = 0, 3
 delta_v = 1, 4
@@ -12,6 +13,10 @@ vectors = dict()
 
 def to_binary_list(number: int) -> Tuple[int, ...]:
     return tuple([int(i) for i in "{0:06b}".format(number)])
+
+
+def negate(or_state):
+    return tuple((1 - gate for gate in or_state))
 
 
 def get_delta(state: Tuple[int, ...], delta: Tuple[int, int]) -> float:
@@ -62,6 +67,8 @@ class BigSector:
     class Point:
         def __init__(self, origin: Tuple[int, ...], end: List[Tuple[int, ...]]):
             al, bet, _ = clark(np.array(get_uvw_from_state(origin)))
+            self.origin_state = origin
+            self.end_states = end
             self.origin = al, bet
             for state in end:
                 assert get_uvw_from_state(end[0]) == get_uvw_from_state(state)
@@ -74,29 +81,63 @@ class BigSector:
             coordinates = [str(Fraction(i).limit_denominator(3)) for i in self.end]
             return f"States {self.states}, located: {coordinates}"
 
+        def states_str(self):
+            state_format = "({},{},{})({},{},{})"
+            state_str = ""
+            counter = 0
+            for state in self.states:
+                state_str += state_format.format(*state) + "\n"
+                counter += 1
+            return state_str
+
+        def symmetric(self):
+
+            neg_or = negate(self.origin_state)
+            neg_end = [negate(state) for state in self.end_states]
+            return BigSector.Point(neg_or, neg_end)
+
     def __init__(self):
         self.points = []
-        point = self.Point
+        point_builder = self.Point
         s1 = (0, 0, 0, 0, 0, 1)
         s2 = (0, 1, 0, 0, 1, 1)
-        self.points.append(point(self.zero, [s1, s2]))
+        self.points.append(point_builder(self.zero, [s1, s2]))
 
         s1 = (0, 0, 0, 0, 1, 1)
         s2 = (1, 0, 0, 0, 0, 0)
-        self.points.append(point(self.zero, [s1, s2]))
+        self.points.append(point_builder(self.zero, [s1, s2]))
 
         s1 = (1, 0, 0, 0, 0, 1)
         s2 = (1, 1, 0, 0, 1, 1)
-        self.points.append(point(self.zero, [s1, s2]))
+        self.points.append(point_builder(self.points[-1].states[0], [s1, s2]))
 
         s1 = (1, 1, 0, 0, 0, 1)
-        self.points.append(point(self.zero, [s1]))
+        self.points.append(point_builder((0, 0, 0, 0, 0, 1), [s1]))
 
         s1 = (1, 0, 0, 0, 1, 1)
-        self.points.append(point(self.zero, [s1]))
+        self.points.append(point_builder((0, 0, 0, 0, 1, 1), [s1]))
 
 
 sector = BigSector()
-
+lims = (-1.75, 1.75)
+plt.xlim(lims)
+plt.ylim(lims)
+i = 1
 for point in sector.points:
-    print(point)
+    plt.arrow(point.origin[0], point.origin[1], point.end[0] - point.origin[0], point.end[1] - point.origin[1],
+              ls=':', color="black")
+    plt.annotate(str(i) + ":\n" + point.states_str(), xy=(point.end[0], point.end[1]))
+    plt.plot(point.end[0], point.end[1], marker="o", color="black")
+    print(i, point)
+
+    point2 = point.symmetric()
+
+    plt.arrow(point2.origin[0], point2.origin[1], point2.end[0] - point2.origin[0], point2.end[1] - point2.origin[1],
+              linestyle='--', color="red")
+    plt.annotate(str(i) + "':\n" + point2.states_str(), xy=(point2.end[0], point2.end[1]))
+    plt.plot(point2.end[0], point2.end[1], marker="o", color="red")
+    print(str(i) + "'", point2)
+
+    i += 1
+
+plt.show()
