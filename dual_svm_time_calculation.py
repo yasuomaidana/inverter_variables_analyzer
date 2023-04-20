290c71f17cf14a6f16f87d2f12ff5e98aaf647d9
yasuomaidana <yasuomaidana@gmail.com>
2023-04-20 15:17:06 +0900

    COUR-982: Implementing find component method.

--- dual_svm_time_calculation.py
+++ dual_svm_time_calculation.py
@@ -1,30 +1,109 @@
-import numpy
-from numpy import arctan2, radians
+import numpy as np
 from typing import Tuple, Optional
+from matplotlib import pyplot as plt
+from sector_symmetry import PointFromNumber, Point, VoltageLevelVector, Vector
 
-from sector_symmetry import BigSector
+point0 = PointFromNumber(0)
+point1 = PointFromNumber((1, 19, 37, 48, 55, 57))
+point2 = PointFromNumber((3, 32, 39, 41, 50, 59))
+point3 = PointFromNumber(35)
+point4 = PointFromNumber((33, 51))
+point5 = PointFromNumber(49)
 
 
-class BigSectorWithCalculator(BigSector):
+class Space:
+    def __init__(self, point_a: PointFromNumber, point_b: PointFromNumber, point_c: PointFromNumber):
+        self.A = point_a
+        self.B = point_b
+        self.C = point_c
+        ab = VoltageLevelVector(point_a, point_b)
+        ac = VoltageLevelVector(point_a, point_c)
+        bc = VoltageLevelVector(point_b, point_c)
+        self.voltage_vectors = [ab, ac, bc]
 
-    def __init__(self, lower_angle=0.0, upper_angle=60.0):
-        super().__init__()
-        self.sub_sectors = [(0, 1, 2), (1, 3, 2), (1, 4, 3), (2, 3, 5)]
-        self.lower_angle = radians(lower_angle)
-        self.upper_angle = radians(upper_angle)
+        self.space = np.vstack((ab.to_array().reshape(1, -1), ac.to_array().reshape(1, -1))).reshape(2, 2).transpose()
 
-    def find_times(self, alpha: float, beta: float) -> Optional[Tuple[int, int]]:
-        theta = arctan2(beta, alpha)
-        theta = theta if theta > 0 else theta + 2 * numpy.pi
+    def calculate_components(self, point: Point) -> Tuple[float, float]:
+        u, v = np.linalg.solve(self.space, Vector(self.A, point).to_array())
+        return u[0], v[0]
 
-        if theta > self.upper_angle or theta < self.lower_angle:
+    def __str__(self):
+        return f"A {self.A}\nB {self.B}\nC {self.C}"
+
+    def plot(self, plotter=plt, color="black", ls=":", marker="o", end_message=None):
+        for vector in self.voltage_vectors:
+            vector.plot(plotter=plotter, color=color, ls=ls, marker=marker, end_message=end_message)
+        self.A.plot(plotter, color, marker)
+
+
+class Sector(Space):
+    zero = PointFromNumber(0)
+
+    def __init__(self, p1, p2, p3, p4, p5):
+        z = self.zero
+        super().__init__(z, p5, p3)
+        self.sub_sectors = [Space(z, p2, p1), Space(p2, p3, p4), Space(p1, p2, p4), Space(p1, p4, p5)]
+
+    def plot(self, plotter=plt, color="black", ls=":", marker="o", end_message=None):
+        for sector in self.sub_sectors:
+            sector.plot(plotter, color, ls, marker, end_message)
+
+    def find_components(self, point: Point) -> Optional[Tuple[float, float, Space]]:
+        t1, t2 = self.calculate_components(point)
+        if t1 > 1 or t2 > 1 or t1 < 0 or t2 < 0:
             return None
+        for sub_sector in self.sub_sectors:
+            t1, t2 = sub_sector.calculate_components(point)
+            if t1 + t2 > 1 or t1 < 0 or t2 < 0:
+                continue
+            return t1, t2, sub_sector
 
-        return 0, 0
+
+def check_in_sector(sub_sector: Sector, point_to_test: Point):
+    found = sub_sector.find_components(point_to_test)
+    if found is None:
+        point_to_test.plot(marker="x", color="red", point_message="outside")
+        return
+    u, v, sect = found
+    u_component = sect.voltage_vectors[0] * u
+    v_component = sect.voltage_vectors[1] * v
+    u_component.plot(color="blue", marker=None)
+    found = v_component.concatenate(u_component)
+    found.plot(color="blue")
+    found.end.plot(color="blue", point_message=f"({found.end.alpha:0.2f},{found.end.beta:0.2f})\nv1:{u:0.3f} v2:{v:0.3f}")
 
 
-sector = BigSectorWithCalculator()
-test = 0.5, .5
-assert sector.find_times(*test) is not None
-test = 0, -.5
-assert sector.find_times(*test) is None
+if __name__ == "__main__":
+    sub_sector_test = Sector(point1, point2, point3, point4, point5)
+    sub_sector_test.plot()
+
+    x = Point(0.1, 0.2)
+    check_in_sector(sub_sector_test, x)
+
+    x2 = Point(point1.alpha * .2, point1.beta * .6)
+    check_in_sector(sub_sector_test, x2)
+
+    x3 = Point(point1.alpha * .5, point1.beta * .5)
+    check_in_sector(sub_sector_test, x3)
+
+    x4 = Point(0.75, 0.4)
+    check_in_sector(sub_sector_test, x4)
+
+    x4 = Point(0.75, 0.8)
+    check_in_sector(sub_sector_test, x4)
+
+    check_in_sector(sub_sector_test, point5)
+
+    x6 = Point(1.25, 0.2)
+    check_in_sector(sub_sector_test, x6)
+
+    x7 = Point(0.5, 0.2)
+    check_in_sector(sub_sector_test, x7)
+
+    x7 = Point(1.5, 0.8)
+    check_in_sector(sub_sector_test, x7)
+
+    x9 = Point(1.5, -0.3)
+    check_in_sector(sub_sector_test, x9)
+
+    plt.show()
